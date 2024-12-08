8dd655dfd2128d4de1e9b009fbf6dfe4a4364890
Simao-dev <pedrosimaocontato@gmail.com>
2024-12-08 18:50:42 -0300

    concluido o html, iniciado a parte de javascript

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const item = document.getElementById("item-input");
+const botaoSalvarItem = document.getElementById("adicionar-botao");
+
+botaoSalvarItem.addEventListener("click", adicionarItem);
+
+function adicionarItem(){
+    console.log("test")
+}
